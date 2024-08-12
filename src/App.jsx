de6de378f4a6d332cc83196eca3049ae41e6f1fe
saran <sarannatarajan7@gmail.com>
2024-08-12 10:39:51 +0530

    again fix

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,7 @@ const useGSAPAnimation = (mainRef, reactRef, skillsRef, ContactsRef) => {
           initialScale = 0.635;
           toScale = 0.25;
         } else if (isMobile) {
-          adjustValue = 235;
+          adjustValue = 210;
           initialScale = 0.415;
           toScale = 0.2;
         } else if (isSmallMobile) {
